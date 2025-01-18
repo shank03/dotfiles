fd0dc25d3e9723be9cec46ab29c04198695df9b6
Shashank Verma <shashank.verma2002@gmail.com>
2025-01-18 18:36:42 +0530

    Add my config
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,22 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+
+return {
+  'ThePrimeagen/vim-be-good',
+  'williamboman/mason.nvim',
+  'rebelot/kanagawa.nvim',
+  'sho-87/kanagawa-paper.nvim',
+  'mg979/vim-visual-multi',
+  'AlexvZyl/nordic.nvim',
+  'EdenEast/nightfox.nvim',
+
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    opts = {
+      direction = 'float',
+      open_mapping = [[<leader>j]],
+    },
+  },
+}
--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -8,17 +8,79 @@ return {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
+    '3rd/image.nvim',
   },
   cmd = 'Neotree',
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '<leader>g', ':Neotree git_status<CR>', desc = 'NeoTree git status', silent = true },
+    { '<leader>b', ':Neotree buffers<CR>', desc = 'NeoTree buffers', silent = true },
   },
   opts = {
+    window = {
+      position = 'right',
+      width = 32,
+    },
+    default_component_configs = {
+      git_status = {
+        symbols = {
+          -- Change type
+          added = '✚',
+          modified = '',
+          deleted = '✖',
+          renamed = 'R',
+          -- Status type
+          untracked = 'N',
+          ignored = '-',
+          unstaged = 'U',
+          staged = 'S',
+          conflict = 'C',
+        },
+        align = 'left',
+      },
+    },
     filesystem = {
+      filtered_items = {
+        visible = true,
+      },
       window = {
         mappings = {
           ['\\'] = 'close_window',
         },
+      },
+    },
+    icon = {
+      folder_closed = '',
+      folder_open = '',
+      folder_empty = '󰜌',
+      provider = function(icon, node, state) -- default icon provider utilizes nvim-web-devicons if available
+        if node.type == 'file' or node.type == 'terminal' then
+          local success, web_devicons = pcall(require, 'nvim-web-devicons')
+          local name = node.type == 'terminal' and 'terminal' or node.name
+          if success then
+            local devicon, hl = web_devicons.get_icon(name)
+            icon.text = devicon or icon.text
+            icon.highlight = hl or icon.highlight
+          end
+        end
+      end,
+      -- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
+      -- then these will never be used.
+      default = '*',
+      highlight = 'NeoTreeFileIcon',
+    },
+    modified = {
+      symbol = '[+]',
+      highlight = 'NeoTreeModified',
+    },
+    git_status = {
+      window = {
+        position = 'float',
+      },
+    },
+    buffers = {
+      window = {
+        position = 'float',
       },
     },
   },
