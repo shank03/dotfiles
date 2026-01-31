a13a2c96fe9d968f77853e797a24fd28e83dc5c9
Shashank Verma <shashank.verma2002@gmail.com>
2026-01-31 21:54:18 +0530

    init: fix enter auto-completion and `s` behavior
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- init.lua
+++ init.lua
@@ -773,11 +773,7 @@ require('lazy').setup({
         -- See :h blink-cmp-config-keymap for defining your own keymap
         preset = 'default',
 
-        ['<CR>'] = {
-          function(cmp)
-            cmp.accept_and_enter()
-          end,
-        },
+        ['<CR>'] = { 'accept', 'fallback' },
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
@@ -859,7 +855,7 @@ require('lazy').setup({
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
+      -- require('mini.surround').setup()
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
