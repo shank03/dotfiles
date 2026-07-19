44ec327fd1ae12b963bc417a3f360c66c2f69fd0
Shashank Verma <shashank.verma2002@gmail.com>
2026-07-20 02:31:10 +0530

    nvim: add markdown renderer
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -18,6 +18,15 @@ return {
   'ThePrimeagen/vim-be-good',
   'williamboman/mason.nvim',
   'mg979/vim-visual-multi',
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
 
   -- themes
   'rebelot/kanagawa.nvim',
