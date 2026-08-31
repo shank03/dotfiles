c6b26fb2b61e8a243bdf46db9349bc5e53935fd8
Shashank Verma <shashank.verma2002@gmail.com>
2026-09-01 00:29:52 +0530

    nvim: add tabs
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

---
+++ nvim/lua/custom/plugins/bufferline.lua
@@ -0,0 +1,19 @@
+return {
+  'akinsho/bufferline.nvim',
+  version = '*',
+  dependencies = 'nvim-tree/nvim-web-devicons',
+  event = 'VeryLazy',
+  opts = {
+    options = {
+      diagnostics = 'nvim_lsp', -- show LSP error/warn count on each tab
+      show_buffer_close_icons = false, -- minimal: no per-tab close 'x'
+      show_close_icon = false,
+      separator_style = 'thin',
+    },
+  },
+  keys = {
+    { '<S-l>', '<cmd>BufferLineCycleNext<cr>', desc = 'Next buffer tab' },
+    { '<S-h>', '<cmd>BufferLineCyclePrev<cr>', desc = 'Prev buffer tab' },
+    { '<leader>x', '<cmd>bdelete<cr>', desc = 'Close buffer' },
+  },
+}
